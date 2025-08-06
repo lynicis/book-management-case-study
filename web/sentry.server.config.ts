a579d01069faabe60f349abb4f0c8716e83bf7c5
İsmail Emre Sırmalı <lynicis@protonmail.com>
2025-08-06 16:24:49 +0300

    chore: init project

---
+++ web/sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e0b3b86897dcec1dc0afcf0b9cd1ad59@o522329.ingest.us.sentry.io/4509797368725504",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
